beb3a6651b910df383e608ee7bafc756b2e657e7
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-04-08 08:42:47 -0700

    index schema v0.2.0
    Keywords now map to an array of keys.

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,5 +1,5 @@
-// TODO: Flip index schema so keywords point to objects
 // TODO: Actually filter out all of the special characters and determiners, prepositions, conjunctions, etc.
+// TODO: Singularize keywords (currently 'science' and 'sciences' are two separate keywords)
 
 const fs = require('fs');
 const os = require('os');
@@ -32,92 +32,115 @@ function keywordizeName(s) {
         .filter((name) => name.length > 1);
 }
 
+// add object to set if keyword exists, create new set if not
+function associate(d, k, o) {
+    Object.keys(d).includes(k) ? d[k].add(o) : (d[k] = new Set([o]));
+}
+
 // parse the data into the format we want, and write it to the output
 function parseAndWriteData(d) {
     // GE categories
     let parsedData = {
-        'GE-1A': {
-            type: 'GE_CATEGORY',
-            name: 'Lower Division Writing',
-        },
+        keywords: {},
+        objects: {
+            'GE-1A': {
+                type: 'GE_CATEGORY',
+                name: 'Lower Division Writing',
+            },
 
-        'GE-1B': {
-            type: 'GE_CATEGORY',
-            name: 'Upper Division Writing',
-        },
+            'GE-1B': {
+                type: 'GE_CATEGORY',
+                name: 'Upper Division Writing',
+            },
 
-        'GE-2': {
-            type: 'GE_CATEGORY',
-            name: 'Science and Technology',
-        },
+            'GE-2': {
+                type: 'GE_CATEGORY',
+                name: 'Science and Technology',
+            },
 
-        'GE-3': {
-            type: 'GE_CATEGORY',
-            name: 'Social and Behavioral Sciences',
-        },
+            'GE-3': {
+                type: 'GE_CATEGORY',
+                name: 'Social and Behavioral Sciences',
+            },
 
-        'GE-4': {
-            type: 'GE_CATEGORY',
-            name: 'Arts and Humanities',
-        },
+            'GE-4': {
+                type: 'GE_CATEGORY',
+                name: 'Arts and Humanities',
+            },
 
-        'GE-5A': {
-            type: 'GE_CATEGORY',
-            name: 'Quantitative Literacy',
-        },
+            'GE-5A': {
+                type: 'GE_CATEGORY',
+                name: 'Quantitative Literacy',
+            },
 
-        'GE-5B': {
-            type: 'GE_CATEGORY',
-            name: 'Formal Reasoning',
-        },
+            'GE-5B': {
+                type: 'GE_CATEGORY',
+                name: 'Formal Reasoning',
+            },
 
-        'GE-6': {
-            type: 'GE_CATEGORY',
-            name: 'Language other than English',
-        },
+            'GE-6': {
+                type: 'GE_CATEGORY',
+                name: 'Language other than English',
+            },
 
-        'GE-7': {
-            type: 'GE_CATEGORY',
-            name: 'Multicultural Studies',
-        },
+            'GE-7': {
+                type: 'GE_CATEGORY',
+                name: 'Multicultural Studies',
+            },
 
-        'GE-8': {
-            type: 'GE_CATEGORY',
-            name: 'International/Global Issues',
+            'GE-8': {
+                type: 'GE_CATEGORY',
+                name: 'International/Global Issues',
+            },
         },
     };
 
-    for (const [key, value] of Object.entries(parsedData)) {
-        parsedData[key].keywords = keywordize(value.name);
+    for (const [key, value] of Object.entries(parsedData.objects)) {
+        for (const keyword of keywordize(value.name)) {
+            associate(parsedData.keywords, keyword, key);
+        }
     }
 
     // departments
-    for (const course of Object.values(d.courses)) {
-        if (Object.keys(parsedData).includes(course.department)) continue;
-        parsedData[course.department] = {
+    for (const value of Object.values(d.courses)) {
+        if (Object.keys(parsedData.objects).includes(value.department)) continue;
+        parsedData.objects[value.department] = {
             type: 'DEPARTMENT',
-            name: course.department_name,
-            keywords: [...course.department_alias, ...keywordize(course.department_name)],
+            name: value.department_name,
         };
+        for (const keyword of [
+            ...value.department_alias.map((x) => x.toLowerCase()),
+            ...keywordize(value.department_name),
+        ]) {
+            associate(parsedData.keywords, keyword, value.department);
+        }
     }
 
     // courses
-    for (const [id, data] of Object.entries(d.courses)) {
-        parsedData[id] = {
+    for (const [key, value] of Object.entries(d.courses)) {
+        parsedData.objects[key] = {
             type: 'COURSE',
-            department: data.department,
-            number: data.number,
-            keywords: keywordizeName(data.title),
+            department: value.department,
+            number: value.number,
         };
+        for (const keyword of keywordize(value.title)) {
+            associate(parsedData.keywords, keyword, key);
+        }
     }
 
     // instructors
     for (const instructor of Object.values(d.instructors)) {
-        parsedData[instructor.shortened_name] = {
+        parsedData.objects[instructor.shortened_name] = {
             type: 'PROFESSOR',
             name: instructor.name,
-            keywords: keywordizeName(instructor.name),
         };
+        for (const keyword of keywordizeName(instructor.name)) {
+            associate(parsedData.keywords, keyword, instructor.shortened_name);
+        }
+    }
+
+    for (const [key, value] of Object.entries(parsedData.keywords)) {
+        parsedData.keywords[key] = [...value];
     }
     fs.writeFileSync(`${localPrefix}${outputFile}`, JSON.stringify(parsedData));
     console.log(`Wrote index to file ${outputFile}`);
