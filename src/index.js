dc2e2314066d55bc74d30cd3546a0bc5bf3e97ed
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-04-08 15:13:52 -0700

    slightly better implementation of fuzzy search
    Prioritizes exact matches over partial matches, so queries for 'ge' will
    actually get all of the GE categories instead of random courses or
    professors who have the substring 'ge' somewhere in their names.

--- src/index.js
+++ src/index.js
@@ -17,23 +17,37 @@ function search(query, numResults = 10) {
     if (query.length < 2) {
         throw new Error('Query must be at least two characters long.');
     }
-    const keyArrs = JSON.parse(
+    const keyArrMap = JSON.parse(
         JSON.stringify(
             Object.keys(index.keywords)
                 .filter((x) => x.includes(query))
                 .sort((a, b) => (a.length === b.length ? (a === b ? 0 : a < b ? -1 : 1) : a.length < b.length ? -1 : 1))
-                .map((x) => index.keywords[x])
+                .reduce((obj, val) => {
+                    obj[val] = index.keywords[val];
+                    return obj;
+                }, {})
         )
     );
-    let response = [];
-    if (!keyArrs.length) return response;
-    while (true) {
-        for (let arr of keyArrs) {
-            if (response.length === numResults) return response;
-            let key = arr.shift();
-            if (key !== undefined) response.push(index.objects[key]);
+    let response = {};
+    if (!Object.keys(keyArrMap).length) return response;
+    for (const key of Object.keys(JSON.parse(JSON.stringify(keyArrMap)))) {
+        if (key === query) {
+            response = keyArrMap[key].slice(0, numResults - 1).reduce((obj, val) => {
+                obj[val] = index.objects[val];
+                return obj;
+            }, {});
+            delete keyArrMap[key];
+            break;
         }
     }
+    while (Object.values(keyArrMap).some((x) => x.length)) {
+        for (let arr of Object.values(keyArrMap)) {
+            if (Object.keys(response).length === numResults) return response;
+            let key = arr.shift();
+            if (key !== undefined) response[key] = index.objects[key];
+        }
+    }
+    return response;
 }
 
 module.exports = { init, search };
