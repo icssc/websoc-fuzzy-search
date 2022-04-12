fff888c5982d26f16995188e65dc8088ce6803e8
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2022-04-11 20:28:37 -0700

    implement multi-word fuzzy searching

--- index.js
+++ index.js
@@ -47,6 +47,58 @@ function expandResponse(response, numResults) {
         }, {});
 }
 
+// search on a single keyword
+function searchSingle(keyword, numResults) {
+    keyword = keyword.toLowerCase();
+    let response = [];
+    // match course codes first if the query contains numbers
+    if (keyword.match(/^[a-z]+[0-9]+[0-9]*$/)) {
+        keyword = keyword.replace(' ', '');
+        for (const [alias, department] of Object.entries(index.aliases)) {
+            keyword = keyword.replace(new RegExp(`^${alias}`), department.toString());
+        }
+        response.push(...Object.keys(index.objects).filter((x) => x.includes(keyword.replace(' ', '').toUpperCase())));
+    }
+    // match all keywords
+    const keyArrMap = Object.keys(index.keywords)
+        .filter((x) => x.includes(keyword))
+        .sort((a, b) => (a.length === b.length ? (a === b ? 0 : a < b ? -1 : 1) : a.length < b.length ? -1 : 1))
+        .reduce((obj, val) => {
+            obj[val] = index.keywords[val];
+            return obj;
+        }, {});
+    // prioritize exact keyword matches
+    let exactDeptMatch = false;
+    for (const kw of Object.keys(keyArrMap)) {
+        if (kw === keyword) {
+            response.push(...keyArrMap[kw]);
+            for (const key of keyArrMap[kw]) {
+                // prioritize exact department matches
+                if (
+                    index.objects[key].type === 'DEPARTMENT' &&
+                    (keyword.toUpperCase() === key || (index.aliases[keyword] && index.aliases[keyword] === key))
+                ) {
+                    response.push(
+                        ...Object.keys(index.objects).filter((x) => index.objects[x].metadata.department === key)
+                    );
+                    exactDeptMatch = true;
+                }
+            }
+            delete keyArrMap[kw];
+            break;
+        }
+    }
+    // add everything else if no exact department match was found
+    if (!exactDeptMatch) response.push(...Object.values(keyArrMap).flat());
+    // if there are bare departments and not enough responses, add the courses from that department
+    for (const key of response) {
+        if (index.objects[key].type === 'DEPARTMENT' && response.length <= numResults) {
+            response.push(...Object.keys(index.objects).filter((x) => index.objects[x].metadata.department === key));
+        }
+    }
+    return [...new Set([...response])];
+}
+
 // load the index into memory
 async function init() {
     const response = await fetch(indexURL);
@@ -61,57 +113,26 @@ function search(query, numResults = 10) {
     if (!initialized) {
         throw new Error('Index has not been initialized. Please run init() before executing a query.');
     }
-    if (query.length < 2) {
-        throw new Error('Query must be at least two characters long.');
-    }
     query = query.toLowerCase();
-    let response = [];
-    // match course codes first if the query contains numbers
-    if (query.match(/^[a-z]+[0-9]+$/)) {
-        query = query.replace(' ', '');
-        for (const [alias, department] of Object.entries(index.aliases)) {
-            query = query.replace(new RegExp(`^${alias}`), department.toString());
-        }
-        response.push(...Object.keys(index.objects).filter((x) => x.includes(query.replace(' ', '').toUpperCase())));
+    // match course code with space, remove the space if detected
+    if (query.match(/([a-z]+) ([0-9]+[a-z]*)/)) {
+        query = query.replace(/([a-z]+) ([0-9]+[a-z]*)/, '$1$2');
     }
-    // match all keywords
-    const keyArrMap = Object.keys(index.keywords)
-        .filter((x) => x.includes(query))
-        .sort((a, b) => (a.length === b.length ? (a === b ? 0 : a < b ? -1 : 1) : a.length < b.length ? -1 : 1))
-        .reduce((obj, val) => {
-            obj[val] = index.keywords[val];
-            return obj;
-        }, {});
-    // prioritize exact keyword matches
-    let exactDeptMatch = false;
-    for (const keyword of Object.keys(keyArrMap)) {
-        if (keyword === query) {
-            response.push(...keyArrMap[keyword]);
-            for (const key of keyArrMap[keyword]) {
-                // prioritize exact department matches
-                if (
-                    index.objects[key].type === 'DEPARTMENT' &&
-                    (query.toUpperCase() === key || (index.aliases[query] && index.aliases[query] === key))
-                ) {
-                    response.push(
-                        ...Object.keys(index.objects).filter((x) => index.objects[x].metadata.department === key)
-                    );
-                    exactDeptMatch = true;
-                }
-            }
-            delete keyArrMap[keyword];
-            break;
-        }
+    const keywords = query.split(' ');
+    if (keywords.some((x) => x.length < 2)) {
+        throw new Error('Each keyword of the query must be at least two characters long.');
     }
-    // add everything else if no exact department match was found
-    if (!exactDeptMatch) response.push(...Object.values(keyArrMap).flat());
-    // if there are bare departments and not enough responses, add the courses from that department
-    for (const key of response) {
-        if (index.objects[key].type === 'DEPARTMENT' && response.length <= numResults) {
-            response.push(...Object.keys(index.objects).filter((x) => index.objects[x].metadata.department === key));
-        }
+    // if only one keyword was given, just run a single query
+    if (keywords.length === 1) {
+        return expandResponse(searchSingle(query, numResults), numResults);
     }
-    return expandResponse([...new Set([...response])], numResults);
+    // take the results of all queries and return their intersection
+    return expandResponse(
+        keywords
+            .map((keyword) => searchSingle(keyword, numResults))
+            .reduce((array, response) => array.filter((x) => response.includes(x))),
+        numResults
+    );
 }
 
 export { init, search };
